adc548db0345a3913311f01601bd53d6a1d23cfe
Edgar Aether <EdgarAether@outlook.jp>
2024-11-14 09:27:53 +0900

    rust home

--- posh/Modules/Envs/module/environment.ps1
+++ posh/Modules/Envs/module/environment.ps1
@@ -4,3 +4,6 @@ $Env:XDG_CACHE_HOME = "$Env:USERPROFILE/.cache"
 $Env:XDG_STATE_HOME = "$Env:USERPROFILE/.local/state"
 $Env:XDG_DATA_HOME = "$Env:USERPROFILE/.local/share"
 $Env:XDG_CONFIG_HOME = "$Env:USERPROFILE/.config"
+
+$Env:RUSTUP_HOME = "$Env:USERPROFILE/.local/rustup"
+$Env:CARGO_HOME = "$Env:USERPROFILE/.local/cargo"
