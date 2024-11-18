77633da360e055ebdf87578abd3199062caffeb2
edgar_aether <edgwitr@gmail.com>
2024-11-18 20:58:56 +0900

    Update ssh_init.sh

--- bin/ssh_init.sh
+++ bin/ssh_init.sh
@@ -1,4 +1,4 @@
-ssh-keygen -t ed25519 -C "EdgarAether@outlook.jp"
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -C "EdgarAether@outlook.jp"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
