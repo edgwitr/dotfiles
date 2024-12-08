8402b71a448656d4b9a828b83442be88308221ad
Edgar Aether <EdgarAether@outlook.jp>
2024-12-08 16:15:11 +0900

    gh

--- bin/win_init.ps1
+++ bin/win_init.ps1
@@ -13,5 +13,6 @@ New-Item -ItemType Junction -Target $posh -Path "$env:USERPROFILE\Documents\Powe
 
 New-Item -ItemType Junction -Target "$dotroot\git" -Path "$Env:USERPROFILE\.config\git"
 New-Item -ItemType Junction -Target "$dotroot\nvim" -Path "$Env:USERPROFILE\.config\nvim"
+New-Item -ItemType Junction -Target "$dotroot\gh" -Path "$Env:USERPROFILE\.config\gh"
 New-Item -ItemType Junction -Target "$dotroot\alacritty" -Path "$Env:APPDATA\alacritty"
 New-Item -ItemType Junction -Target "$dotroot\winterm" -Path "$Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
