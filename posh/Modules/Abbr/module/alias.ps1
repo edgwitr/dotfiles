3bfef23f892040f1d75910a2c7663d8e271dbd61
Edgar Aether <EdgarAether@outlook.jp>
2024-12-15 04:25:44 +0900

    git abbr

--- posh/Modules/Abbr/module/alias.ps1
+++ posh/Modules/Abbr/module/alias.ps1
@@ -1,5 +1,5 @@
-﻿Set-Abbr gst 'git status'
-Set-Abbr gcom 'git commit -m "%"' -NoSpaceAfter
+﻿Set-Abbr gia 'git add'
+Set-Abbr gic 'git commit -m "%"' -NoSpaceAfter
 Set-Abbr gsgs 'a "%" b "%" c "%"' -NoSpaceAfter
 Set-Abbr reload '. $PROFILE'
 Set-Abbr d 'Set-Location ~/.local/dotfiles'
