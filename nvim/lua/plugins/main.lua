1bdbfe4ece1f6421ee74f9a34fdc134a7c28a02c
EDOGAWA Iteru <EdgarAether@outlook.jp>
2024-12-23 17:43:21 +0900

    add nvim-plugins
    flash.nvim, oil.nvim

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -20,6 +20,29 @@ return {
     opts = {}
   },
 
+{
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+},
+{
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+},
   -- {
   --   -- buffer line
   --   'akinsho/bufferline.nvim',
