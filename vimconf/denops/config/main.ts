d06f82db7fe44d0fe6da3f715c1b9d5b476880e3
EDOGAWA Iteru <EdgarAether@outlook.jp>
2025-01-08 17:20:18 +0900

    <Left> umaku ikanai save

--- vimconf/denops/config/main.ts
+++ vimconf/denops/config/main.ts
@@ -1,22 +1,46 @@
 import type { Entrypoint } from "jsr:@denops/std@^7.0.0";
-import * as mapping from "jsr:@denops/std@^7.0.0/mapping";
+import * as option from "jsr:@denops/std@^7.0.0/option";
+import * as fn from "jsr:@denops/std@^7.0.0/function";
+import * as buffer from "jsr:@denops/std@^7.0.0/buffer";
+import { assert, is } from "jsr:@core/unknownutil@^4.3.0";
 
-const dictionary: Readonly<Record<string, number>> = {
-  "key1": 100,
-  "key2": 200,
-  "(":300,
-};
 
 export const main: Entrypoint = async (denops) => {
-  await mapping.map(denops, "<C-S>", "<CMD>call denops#request('config', 'stst', [])<CR>", { mode: "i" },);
+  await Promise.all([
+    option.number.set(denops, true),
+    option.relativenumber.set(denops, true),
+    option.breakindent.set(denops, true),
+    option.autoindent.set(denops, true),
+    option.smartindent.set(denops, true),
+    option.smarttab.set(denops, true),
+    option.expandtab.set(denops, true),
+    option.signcolumn.set(denops, "yes"),
+    option.shiftwidth.set(denops, 2),
+    option.list.set(denops, true),
+    option.listchars.set(denops, "tab:>>,trail:-,nbsp:+,eol:$"),
+    denops.cmd(`command! -nargs=? Hello call denops#request("${denops.name}", "hello", [<q-args>])`),
+  ]);
+
   denops.dispatcher = {
-    async sss(): Promise<void> {
-      console.log("Plugin is ready!");
-      console.log(dictionary["key1"]);
-    },
-    stst(): void {
-      console.log("Plugin is ready!");
-      console.log(dictionary["key2"]);
+    async hello(name) {
+      assert(name, is.String);
+      await denops.cmd(`echo "Hello, ${name || 'Denops'}!"`);
     },
   };
+
+  // const area = "130000";
+  // const res = await fetch(
+  //   `https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${area}.json`,
+  // );
+  // const msg = [
+  //   "☆東京の天気情報☆",
+  //   "------------------------------",
+  //   "",
+  //   ...(await res.json()).text.split("\n"),
+  // ];
+  // const buf = await buffer.open(denops, "forecast");
+  // await fn.setbufvar(denops, buf.bufnr, "&buftype", "nofile");
+  // await fn.setbufvar(denops, buf.bufnr, "&swapfile", 0);
+  // await buffer.replace(denops, buf.bufnr, msg);
+  // await buffer.concrete(denops, buf.bufnr);
 };
--- vimconf/denops/test/main.ts
+++ vimconf/denops/test/main.ts
@@ -1,45 +1,11 @@
 import type { Entrypoint } from "jsr:@denops/std@^7.0.0";
-import * as option from "jsr:@denops/std@^7.0.0/option";
-import * as fn from "jsr:@denops/std@^7.0.0/function";
-import * as buffer from "jsr:@denops/std@^7.0.0/buffer";
-import { assert, is } from "jsr:@core/unknownutil@^4.3.0";
-
+import * as mapping from "jsr:@denops/std@^7.0.0/mapping";
 
 export const main: Entrypoint = async (denops) => {
-  await option.number.set(denops, true);
-  await option.relativenumber.set(denops, true);
-  await option.breakindent.set(denops, true);
-  await option.autoindent.set(denops, true);
-  await option.smartindent.set(denops, true);
-  await option.smarttab.set(denops, true);
-  await option.expandtab.set(denops, true);
-  await option.signcolumn.set(denops, "yes");
-  await option.shiftwidth.set(denops, 2);
-  await option.list.set(denops, true);
-  await option.listchars.set(denops, "tab:>>,trail:-,nbsp:+,eol:$");
-
-  await denops.cmd(`command! -nargs=? Hello call denops#request("${denops.name}", "hello", [<q-args>])`);
-
   denops.dispatcher = {
-    async hello(name) {
-      assert(name, is.String);
-      await denops.cmd(`echo "Hello, ${name || 'Denops'}!"`);
+    async sss(): Promise<void> {
+    },
+    stst(): void {
     },
   };
-
-  // const area = "130000";
-  // const res = await fetch(
-  //   `https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${area}.json`,
-  // );
-  // const msg = [
-  //   "☆東京の天気情報☆",
-  //   "------------------------------",
-  //   "",
-  //   ...(await res.json()).text.split("\n"),
-  // ];
-  // const buf = await buffer.open(denops, "forecast");
-  // await fn.setbufvar(denops, buf.bufnr, "&buftype", "nofile");
-  // await fn.setbufvar(denops, buf.bufnr, "&swapfile", 0);
-  // await buffer.replace(denops, buf.bufnr, msg);
-  // await buffer.concrete(denops, buf.bufnr);
 };
