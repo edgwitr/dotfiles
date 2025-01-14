282ec109bfb6b2f0f1c33d6bc5db564f96c5c31e
EDOGAWA Iteru <EdgarAether@outlook.jp>
2025-01-14 16:10:17 +0900

    saeve

--- vimconf/denops_/config/main.ts
+++ vimconf/denops_/config/main.ts
@@ -6,27 +6,27 @@ import { assert, is } from "jsr:@core/unknownutil@^4.3.0";
 
 
 export const main: Entrypoint = async (denops) => {
-  await Promise.all([
-    option.number.set(denops, true),
-    option.relativenumber.set(denops, true),
-    option.breakindent.set(denops, true),
-    option.autoindent.set(denops, true),
-    option.smartindent.set(denops, true),
-    option.smarttab.set(denops, true),
-    option.expandtab.set(denops, true),
-    option.signcolumn.set(denops, "yes"),
-    option.shiftwidth.set(denops, 2),
-    option.list.set(denops, true),
-    option.listchars.set(denops, "tab:>>,trail:-,nbsp:+,eol:$"),
-    denops.cmd(`command! -nargs=? Hello call denops#request("${denops.name}", "hello", [<q-args>])`),
-  ]);
+  // await Promise.all([
+  //   option.number.set(denops, true),
+  //   option.relativenumber.set(denops, true),
+  //   option.breakindent.set(denops, true),
+  //   option.autoindent.set(denops, true),
+  //   option.smartindent.set(denops, true),
+  //   option.smarttab.set(denops, true),
+  //   option.expandtab.set(denops, true),
+  //   option.signcolumn.set(denops, "yes"),
+  //   option.shiftwidth.set(denops, 2),
+  //   option.list.set(denops, true),
+  //   option.listchars.set(denops, "tab:>>,trail:-,nbsp:+,eol:$"),
+  //   denops.cmd(`command! -nargs=? Hello call denops#request("${denops.name}", "hello", [<q-args>])`),
+  // ]);
 
-  denops.dispatcher = {
-    async hello(name) {
-      assert(name, is.String);
-      await denops.cmd(`echo "Hello, ${name || 'Denops'}!"`);
-    },
-  };
+  // denops.dispatcher = {
+  //   async hello(name) {
+  //     assert(name, is.String);
+  //     await denops.cmd(`echo "Hello, ${name || 'Denops'}!"`);
+  //   },
+  // };
 
   // const area = "130000";
   // const res = await fetch(
