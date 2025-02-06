6a71973720ce811af1fceb4f9ff4a05603a0a786
EDOGAWA Iteru <EdgarAether@outlook.jp>
2025-02-06 17:27:58 +0900

    guix

---
+++ bin/sh/fedora-rawhide.sh
@@ -0,0 +1,2 @@
+sudo dnf install fedora-repos-rawhide
+sudo dnf --releasever=rawhide distro-sync
---
+++ bin/sh/guix.sh
@@ -0,0 +1,1 @@
+curl "https://git.savannah.gnu.org/gitweb/?p=guix.git;a=blob_plain;f=etc/guix-install.sh;hb=HEAD" > ~/guix-install.sh
